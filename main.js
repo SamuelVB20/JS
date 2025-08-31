7cbb42199b43a46e6ab10f5eccade12daea707a8
Samuel Velasquez <samuelvelasquez1975@gmail.com>
2025-08-31 18:02:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function CambiarTexto1() {
+    const elemento = document.getElementById("h1");
+    elemento.innerText = "Totalmente soleado";
+    document.body.style.background = "yellow"
+
+}
+
+function CambiarTexto2() {
+    const elemento = document.getElementById("h1");
+    elemento.innerText = "lluvioso ";
+    document.body.style.background = "cadetblue"
+
+
+}
+
+function CambiarTexto3() {
+    const elemento = document.getElementById("h1");
+    elemento.innerText = "Nublado";
+    document.body.style.background = "gray"
+
+}
